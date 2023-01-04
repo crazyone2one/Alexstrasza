3b0ca2406b10697df1ca9c8f3a8d024ae9350c3c
jingll <the2ndindec@hotmail.com>
2023-01-04 15:23:11 +0800

    refactor: 增加缓存参数

--- frontend/src/store/modules/app.ts
+++ frontend/src/store/modules/app.ts
@@ -3,6 +3,9 @@ import { ref } from 'vue'
 
 export interface IAppState {
   sidebarCollapsed: boolean
+  currentWorkspaceId: string
+  currentProjectId: string
+  workspaceName: string
 }
 
 export const useAppStore = defineStore(
@@ -10,11 +13,42 @@ export const useAppStore = defineStore(
   () => {
     const app = ref<IAppState>({
       sidebarCollapsed: false,
+      currentWorkspaceId: '',
+      currentProjectId: '',
+      workspaceName: '',
     })
     const setCollapsed = (): void => {
       app.value.sidebarCollapsed = !app.value.sidebarCollapsed
     }
-    return { app, setCollapsed }
+    const setProjectId = (projectId: string): void => {
+      app.value.currentProjectId = projectId
+    }
+    const getProjectId = (): string => {
+      return app.value.currentProjectId
+    }
+    const setWorkspaceId = (workspaceId: string): void => {
+      app.value.currentWorkspaceId = workspaceId
+    }
+
+    const getWorkspaceId = (): string => {
+      return app.value.currentWorkspaceId
+    }
+    const setWorkspaceName = (workspaceName: string): void => {
+      app.value.workspaceName = workspaceName
+    }
+    const getWorkspaceName = (): string => {
+      return app.value.workspaceName
+    }
+    return {
+      app,
+      setCollapsed,
+      setProjectId,
+      setWorkspaceId,
+      getProjectId,
+      getWorkspaceId,
+      setWorkspaceName,
+      getWorkspaceName,
+    }
   },
   {
     persist: true,
