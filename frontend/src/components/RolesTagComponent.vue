fdd4c880d063350f066a2ad1e0378c629852e021
jingll <the2ndindec@hotmail.com>
2023-01-06 15:30:56 +0800

    feat: 权限标签组件

---
+++ frontend/src/components/RolesTagComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { NTag } from 'naive-ui'
+import { ISystemGroup } from '../apis/modules/user-group'
+interface Props {
+  type?: 'default' | 'success' | 'error' | 'primary' | 'info' | 'warning'
+  bordered?: boolean
+  roles: ISystemGroup[]
+}
+withDefaults(defineProps<Props>(), {
+  type: 'success',
+  bordered: false,
+})
+</script>
+<template>
+  <div>
+    <span v-for="(role, index) in roles" :key="index">
+      <n-tag :type="type" :bordered="bordered">{{ role.name }}</n-tag>
+    </span>
+  </div>
+</template>
+
+<style scoped></style>
