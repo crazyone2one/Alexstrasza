18dd3b50b67508219714c5f11b1bc84e365454e3
jingll <the2ndindec@hotmail.com>
2023-01-12 11:40:46 +0800

    feat: 使用mitt

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,14 @@ import './styles'
 import router from './router'
 import pinia from './store'
 import { i18n } from '/@/i18n/index'
+import mitt from 'mitt'
+const Mit = mitt()
 
+declare module 'vue' {
+  export interface ComponentCustomProperties {
+    $Bus: typeof Mit
+  }
+}
 const app = createApp(App)
 app.use(router).use(pinia).use(i18n)
 
@@ -13,4 +20,5 @@ const meta = document.createElement('meta')
 meta.name = 'naive-ui-style'
 document.head.appendChild(meta)
 
+app.config.globalProperties.$Bus = Mit
 app.mount('#app')
