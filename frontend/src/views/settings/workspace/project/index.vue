8bedd3d5a9bb50f8ec580e9fbd7cde213dcf52f5
jingll <the2ndindec@hotmail.com>
2023-01-12 16:22:27 +0800

    refactor: 记录一下

--- frontend/src/views/settings/workspace/project/index.vue
+++ frontend/src/views/settings/workspace/project/index.vue
@@ -117,7 +117,6 @@ const createColumns = (): DataTableColumns<IProject> => {
                 },
                 { default: () => t('commons.edit') }
               ),
-
               h(
                 NPopconfirm,
                 {
