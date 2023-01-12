38180c85b932adb8a3286701c325d061373aa739
jingll <the2ndindec@hotmail.com>
2023-01-12 12:37:12 +0800

    refactor: 配置用户权限

--- backend/src/main/java/cn/master/backend/mapper/SysUserGroupMapper.java
+++ backend/src/main/java/cn/master/backend/mapper/SysUserGroupMapper.java
@@ -8,6 +8,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -22,6 +23,17 @@ import java.util.List;
 @Mapper
 public interface SysUserGroupMapper extends BaseMapper<SysUserGroup> {
     List<RelatedSource> getRelatedSource(@Param("userId") String userId);
+
     List<SysGroup> getWorkspaceMemberGroups(@Param("workspaceId") String workspaceId, @Param("userId") String userId);
+
     List<UserGroupDTO> getUserGroup(@Param("userId") String userId, @Param("projectId") String projectId);
+
+    /**
+     * desc: 查询用户权限
+     *
+     * @param userId user id
+     * @return java.util.List<java.lang.String>
+     */
+    @Select("select distinct group_id from sys_user_group where user_id=#{userId}")
+    List<String> getUserGroupByUserId(@Param("userId") String userId);
 }
